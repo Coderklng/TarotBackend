a9030553dca7853e844b9876bce71b54d8172069
Coderklng <kkartiksharma999@gmail.com>
2026-08-18 09:43:50 +0530

    changes server.js file

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 });
 
 // 7. Server Initialization
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port: ${PORT}`);
 });
